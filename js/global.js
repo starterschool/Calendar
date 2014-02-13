9074347b89b01cd3fd819ed11fa0d66bbb02a015
Sandor Weisz <santheo@mac.com>
2014-02-13 17:36:45 -0600

    Built out the beginnings of the insertion logic. Hard coded the 'feb' for now.

--- js/global.js
+++ js/global.js
@@ -8,8 +8,26 @@ $(function () {
     $.getJSON(
         "/data.json",
         function(data) {
-            $.each(data,function(i,item) {
-                console.log(i, item);
+            $.each(data,function(i, item) {
+                // Create a new div to contain our markup for the event
+                var el = $('<div></div>').addClass('event');
+
+                // Create HTML elements to go in the .event
+                var title = $('<h3></h3>').html(item.title);
+                var desc = $('<p></p>').html(item.description);
+                // … plus a bunch more
+
+                // Parse the Date
+                var date = new Date(item.entry_date);
+                var dateNum = date.getDate();
+                var dateID = "feb" + dateNum;
+
+                // Build up the .event object
+                el.append(title);
+                el.append(desc);
+
+                // Add it to the proper date in the calendar
+                $('#' + dateID).append(el);
             });
         }
     );
