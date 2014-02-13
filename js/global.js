f8c7ce6bc717453d6d4f2ba1cf6c45522ed85114
Sandor Weisz <santheo@mac.com>
2014-02-13 14:28:32 -0600

    Added, styled event.

--- js/global.js
+++ js/global.js
@@ -1,4 +1,7 @@
 // Execute the following code when the page is loaded
 $(function () {
-
+    $('.event h3 a').click(function(e) {
+        e.preventDefault();
+        $(this).closest('.event').find('p').show();
+    });
 });
